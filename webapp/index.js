bf6d0ca089167b759b455ce08a24e2658ac0d8d3
Petra Linaru <petrarozalia@yahoo.com>
2023-07-03 17:55:56 +0300

    Made it till step 24 in tutorial

---
+++ webapp/index.js
@@ -0,0 +1,14 @@
+//app logic
+sap.ui.define([
+    "sap/ui/core/ComponentContainer"
+], function(ComponentContainer) {
+    "use strict";
+    
+    new ComponentContainer({
+        name: "sap.ui.demo.walkthrough",
+        settings: {
+            id: "walkthrough"
+        },
+        async: true
+    }).placeAt("content");
+});
